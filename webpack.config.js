76b612b763221f565223e9ada0865ef84b583b31
Takashi Motoki <t.motoki01@gmail.com>
2019-02-13 22:14:46 +0900

    npmを最新に更新+Babel7を導入
    $ npm install -g npm-check-updates
    $ npm-check-updates -u
    $ npm update
    $ npm -g install babel-upgrade
    $ babel-upgrade --write --install
    $ npm i -D webpack webpack-cli
    
    webpack.configを2以上の書き方に変更

--- webpack.config.js
+++ webpack.config.js
@@ -18,34 +18,50 @@ var config = {
   },
 
   module : {
-    loaders : [
+    rules : [
       {
         test: /\.(jpe?g|png|gif)$/i,   //to support eg. background-image property
-        loader:"file-loader",
-        query:{
-          name:'[path][name].[ext]',
-          outputPath: '../'
-        }
+        use:[
+          {
+            loader: "file-loader",
+            options:{
+              name:'[path][name].[ext]',
+              outputPath: '../'
+            }
+          }
+        ]
       },{
         test: /\.(woff(2)?|ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,    //to support @font-face rule
-        loader: "url-loader",
-        query:{
-          limit:'10000',
-          name:'[path][name].[ext]',
-          outputPath: '../'
-        }
+        use: [
+          {
+            loader: "url-loader",
+            options:{
+              limit:'10000',
+              name:'[path][name].[ext]',
+              outputPath: '../'
+            }
+          }
+        ]
       }, {
         test: /\.jsx?$/,
         include : APP_DIR,
         exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'env', 'stage-0'],
-          plugins: ['react-html-attrs', 'transform-decorators-legacy', 'transform-class-properties'],
-        }
+        use:[
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-react', '@babel/preset-env'],
+              plugins: [
+                'react-html-attrs',
+                ['@babel/plugin-proposal-decorators', { "legacy": true }],
+                ['@babel/plugin-proposal-class-properties', { "loose": true }]
+              ]
+            }
+          }
+        ]
       },{
         test: /\.scss$/,
-        loaders: ["style-loader","css-loader","sass-loader"]
+        use: ["style-loader","css-loader","sass-loader"]
       }
     ]
   },
